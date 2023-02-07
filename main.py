5ca2d52bf2e4d0e346b35f22b41397b80273aa62
Gianfranco Liporace <gliporace@gmail.com>
2023-02-07 11:34:40 +0100

    Comments clean-up

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+""" main entrypoint """
 #####################
 #
 # acceweb_new_user
 # Crea un utente Acceweb utilizzando Google Chrome per compilare la maschera di
 # creazione utenti di Hitech Acceweb
-# utilizza il driver ChromeDriver per "pilotare" Google Chrome scaricabile da https://chromedriver.chromium.org
+# utilizza il driver ChromeDriver per "pilotare" Google Chrome scaricabile da
+# https://chromedriver.chromium.org
 # scaricare la versione compatibile con quella di Chrome installato
 # che deve essere posizionato in c:\Python38
 # o in una cartella in PATH
@@ -15,7 +17,6 @@ import wx
 from ui import MainWindow
 
 if __name__ == '__main__':
-    
     app = wx.App()
     frame = MainWindow().Show()
     app.MainLoop()
